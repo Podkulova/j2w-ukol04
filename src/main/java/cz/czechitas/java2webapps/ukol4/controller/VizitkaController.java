1ec1dc67e046bd3bfcd91bad7fb8e892e6b4410f
Dasa <dasa.podkulova@gmail.com>
2024-05-19 13:27:18 +0200

    Hotovo :)

--- src/main/java/cz/czechitas/java2webapps/ukol4/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol4/controller/VizitkaController.java
@@ -1,10 +1,15 @@
-package cz.czechitas.java2webapps.ukol3.controller;
+package cz.czechitas.java2webapps.ukol4.controller;
 
-import cz.czechitas.java2webapps.ukol3.service.VizitkaService;
+import cz.czechitas.java2webapps.ukol4.entity.Vizitka;
+import cz.czechitas.java2webapps.ukol4.service.VizitkaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
@@ -17,10 +22,13 @@ public class VizitkaController {
         this.service = service;
     }
 
+    private final List<Vizitka> seznamVizitek = new ArrayList<>();
+
+
     @GetMapping("/")
     public ModelAndView seznam() {
         ModelAndView result = new ModelAndView("seznam");
-        result.addObject("seznam", service.getAll());
+        result.addObject("vizitky", service.getAll());
         return result;
     }
 
@@ -29,5 +37,17 @@ public class VizitkaController {
         ModelAndView result = new ModelAndView("detail");
         result.addObject("vizitka", service.getById(id));
         return result;
+    }
+
+    @PostMapping("/nova")
+    public String create(Vizitka vizitka) {
+        service.create(vizitka);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete")
+    public String delete(int id) {
+        service.deleteById(id);
+        return "redirect:/";
     }
 }
